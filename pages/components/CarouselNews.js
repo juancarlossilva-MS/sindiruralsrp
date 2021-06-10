437c9bb6722122123b9ef01abbb7fffaffd5199a
Juan Carlos Silva <juancarlossilva@MacBook-Pro-de-Juan.local>
2021-06-09 21:26:09 -0300

    s

---
+++ pages/components/CarouselNews.js
@@ -0,0 +1,170 @@
+import React from "react";
+// react component for creating beautiful carousel
+import Carousel from "react-slick";
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+// @material-ui/icons
+import LocationOn from "@material-ui/icons/LocationOn";
+// core components
+import GridContainer from "components/Grid/GridContainer.js";
+import GridItem from "components/Grid/GridItem.js";
+import Card from "components/Card/Card.js";
+
+import styles from "styles/jss/nextjs-material-kit/pages/componentsSections/carouselStyle.js";
+import Badge from "components/Badge/Badge.js";
+import stylesTypo from "styles/jss/nextjs-material-kit/pages/componentsSections/typographyStyle.js";
+import Small from "components/Typography/Small.js";
+
+const useStyles = makeStyles(styles);
+const useStylesTypo = makeStyles(stylesTypo);
+
+export default function SectionCarousel(props) {
+  const classes = useStyles();
+  const {tipoCar} = props;
+  const classesTypo = useStylesTypo();
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: false,
+  };
+
+  const styleCaption = {
+    paddingBottom: "0px",
+    position: "absolute",
+    right: "0%",
+    bottom: "-3%",
+    left: "0%",
+    zIndex: "10",
+    paddingTop: "0",
+    color: "#ffffff",
+    textAlign: "center",
+    zIndex: "3",
+    display: "block",
+    backgroundColor: "#0000005f",
+
+  }
+
+  const styleImg ={
+    maxWidth: "100%",
+    maxHeight: "20.4rem",
+    minHeight: "20.4rem",
+    minWidth: "100%",
+  }
+
+  if(tipoCar == "cursos"){
+    return (
+        <div >
+          <div className={classes.container} style={{width:"130%"}}>
+    
+            <GridContainer>
+              <GridItem xs={12} sm={12} md={8} >
+                <Card carousel style={{height:"83%"}}>
+                  <Carousel {...settings}>
+                    <div>
+                      <img
+                        src="/img/notice.jpg"
+                        alt="First slide"
+                        style={styleImg}
+                      />
+                      <div style={{padding:"1%"}}>
+                        <Badge color="success">Logística</Badge>
+                        <div className={classesTypo.typo} style={{padding:"0px"}}>
+                        <h4>
+                            COMITÊ teste EXTRAORDINÁRIO DA COVID-19 ATENDE SOLICITAÇÃO DO SINDICATO RURAL
+                            <br />
+                            <Small>O Comitê Extraordinário da Covid-19 atendeu à solicitação do Sindicato Rural de Dourados, realizada no último dia 30, e autorizou que produtores rurais, empresas Seguradoras de Seguros Agrícolas e de Planejamento e Assistência Técnica Rural, bem como seus funcionários e prepostos, realizem os serviços durante o lockdown decretado no município.</Small>
+                        </h4>
+                        </div>
+                        
+                      </div>
+                    </div>
+                    <div>
+                      <img
+                        src="/img/notice2.jpg"
+                        alt="First slide"
+                        style={styleImg}
+                      />
+                      <div style={{padding:"1%"}}>
+                        <Badge color="success">Logística</Badge>
+                        <div className={classesTypo.typo} style={{padding:"0px"}}>
+                        <h4>
+                        NOVA FERROESTE DEVE INICIAR OPERAÇÕES COM TRANSPORTE DE 26 MILHÕES DE TONELADAS DE CARGA
+    
+                            <br />
+                            <Small>No “Ano Zero”, momento em que estiver concluída e iniciando suas operações, a Nova Ferroeste deverá atender...
+                            </Small>
+                        </h4>
+                        </div>
+                        
+                      </div>
+                    </div>
+                    
+                   
+                   </Carousel>
+                </Card>
+              </GridItem>
+            </GridContainer>
+          </div>
+        </div>
+      );
+  }else{
+  return (
+    <div >
+      <div className={classes.container} style={{width:"130%"}}>
+
+        <GridContainer>
+          <GridItem xs={12} sm={12} md={8} >
+            <Card carousel style={{height:"83%"}}>
+              <Carousel {...settings}>
+                <div>
+                  <img
+                    src="/img/notice.jpg"
+                    alt="First slide"
+                    style={styleImg}
+                  />
+                  <div style={{padding:"1%"}}>
+                    <Badge color="success">Logística</Badge>
+                    <div className={classesTypo.typo} style={{padding:"0px"}}>
+                    <h4>
+                        COMITÊ EXTRAORDINÁRIO DA COVID-19 ATENDE SOLICITAÇÃO DO SINDICATO RURAL
+                        <br />
+                        <Small>O Comitê Extraordinário da Covid-19 atendeu à solicitação do Sindicato Rural de Dourados, realizada no último dia 30, e autorizou que produtores rurais, empresas Seguradoras de Seguros Agrícolas e de Planejamento e Assistência Técnica Rural, bem como seus funcionários e prepostos, realizem os serviços durante o lockdown decretado no município.</Small>
+                    </h4>
+                    </div>
+                    
+                  </div>
+                </div>
+                <div>
+                  <img
+                    src="/img/notice2.jpg"
+                    alt="First slide"
+                    style={styleImg}
+                  />
+                  <div style={{padding:"1%"}}>
+                    <Badge color="success">Logística</Badge>
+                    <div className={classesTypo.typo} style={{padding:"0px"}}>
+                    <h4>
+                    NOVA FERROESTE DEVE INICIAR OPERAÇÕES COM TRANSPORTE DE 26 MILHÕES DE TONELADAS DE CARGA
+
+                        <br />
+                        <Small>No “Ano Zero”, momento em que estiver concluída e iniciando suas operações, a Nova Ferroeste deverá atender...
+                        </Small>
+                    </h4>
+                    </div>
+                    
+                  </div>
+                </div>
+                
+               
+               </Carousel>
+            </Card>
+          </GridItem>
+        </GridContainer>
+      </div>
+    </div>
+  );
+}
+}
--- pages/index.js
+++ pages/index.js
@@ -2,54 +2,44 @@ import Head from 'next/head'
 import React from "react";
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
+import { Grid } from "@material-ui/core";
 import Header from './components/Header';
+import CarouselNews from './components/CarouselNews';
 
 export default function Home() {
 
   return (
     <div >
+      <link
+            rel="stylesheet"
+            type="text/css"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons"
+          />
+          <link
+            href="https://use.fontawesome.com/releases/v5.0.10/css/all.css"
+            rel="stylesheet"
+          />
       <Head>
         <title>Site Oficial do Sindicato Rural de Santa Rita do Pardo - MS</title>
         <link rel="icon" href="/logo.png" />
       </Head>
       <Header/>
-      <main >
-        <h1 >
-         Em breve aqui o mais novo do <br/><a href="">SINDICATO RURAL DE SANTA RITA DO PARDO!</a>
-        </h1>
+      
+      <div style={{padding:"0px"}}> 
+        <Grid container  spacing={0}>
+          <Grid item  xs={1}>
+          </Grid>
+          <Grid item  xs={7}>
+              <CarouselNews />
+          </Grid>
+          <Grid item xs={4} >
+              <CarouselNews tipoCar="cursos"/>
+          </Grid>
+        </Grid>
+      </div>
 
-        
 
-        <div >
-          <a href="https://nextjs.org/docs" >
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
 
-          <a href="https://nextjs.org/learn" >
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
 
       
     </div>
