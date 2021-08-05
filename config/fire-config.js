12153c67bbffd6c247e126e3a24201df9ff5a369
Juan Carlos Silva <juancarlossilva@MacBook-Pro-de-Juan.local>
2021-08-05 13:32:43 -0300

    vamosnessa

--- config/fire-config.js
+++ config/fire-config.js
@@ -1,21 +1,40 @@
 import firebase from 'firebase';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDPx6fGmbOJFWeTxJHM2KQRegtbsXOOO3A",
-    authDomain: "sindiruralsrp.firebaseapp.com",
-    databaseURL: "https://sindiruralsrp-default-rtdb.firebaseio.com",
-    projectId: "sindiruralsrp",
-    storageBucket: "sindiruralsrp.appspot.com",
-    messagingSenderId: "130462633564",
-    appId: "1:130462633564:web:ca0ba6459240f179262099"
-};
 
-try {
-  firebase.initializeApp(firebaseConfig);
-} catch(err){
-  if (!/already exists/.test(err.message)) {
-    console.error('Firebase initialization error', err.stack)}
-}
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyDPx6fGmbOJFWeTxJHM2KQRegtbsXOOO3A",
+      authDomain: "sindiruralsrp.firebaseapp.com",
+      databaseURL: "https://sindiruralsrp-default-rtdb.firebaseio.com",
+      projectId: "sindiruralsrp",
+      storageBucket: "sindiruralsrp.appspot.com",
+      messagingSenderId: "130462633564",
+      appId: "1:130462633564:web:ca0ba6459240f179262099"
+  };
+
+  try {
+    firebase.initializeApp(firebaseConfig);
+  } catch(err){
+    if (!/already exists/.test(err.message)) {
+      console.error('Firebase initialization error', err.stack)}
+  }
+
 
 const fire = firebase;
-export default fire;
+export default  fire;
+
+
+export const askForPermissionToReceiveNotifications = async () => {
+  try {
+
+    const messaging = firebase.messaging();
+
+    await messaging.requestPermission();
+    const token = await messaging.getToken();
+    console.log('user token: ', token);
+
+    return token;
+  } catch (error) {
+    console.error(error);
+  }
+}
